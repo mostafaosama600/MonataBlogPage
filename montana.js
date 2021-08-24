7e2cf7c753685ec7a8ea203cbca83d8d7359ba4d
AlaaAziz2020 <87227043+AlaaAziz2020@users.noreply.github.com>
2021-08-25 01:44:54 +0200

    Delete montana.js

--- montana.js
+++
@@ -1,9 +0,0 @@
-var header= document.querySelector('header');
-
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 100) {
-    header.classList.add('bg-dark', 'shadow');
-  } else {
-    header.classList.remove('bg-dark', 'shadow');
-  }
-});
